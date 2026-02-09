b427c158419b9034297424207cf0966c42119cff
Raul Gama <thecureomd@gmail.com>
2026-02-09 01:45:01 +0100

    refactor: remove full CD and production pipelines, update staging pipeline with new structure and stages

--- pipelines/common-steps/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source todo-list-aws/bin/activate
-set -x
-sam validate --region us-east-1
-sam build
--- pipelines/common-steps/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x
-du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
