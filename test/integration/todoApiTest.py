7a3c31b4c173113bc4b2b8678935b80999fb5004
raul gama <thecureomd@gmail.com>
2026-02-11 02:19:45 +0000

    fix(integration): fixed return to the original integration test version

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -27,7 +27,7 @@ class TestApi(unittest.TestCase):
         data = {
          "text": "Integration text example"
         }
-        response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        response = requests.post(url, data=json.dumps(data))
         json_response = response.json()
         print('Response Add Todo: '+ str(json_response))
         jsonbody= json.loads(json_response['body'])
@@ -56,7 +56,7 @@ class TestApi(unittest.TestCase):
         data = {
          "text": "Integration text example"
         }
-        response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        response = requests.post(url, data=json.dumps(data))
         json_response = response.json()
         print('Response Add Todo: '+ json_response['body'])
         jsonbody= json.loads(json_response['body'])
@@ -82,7 +82,7 @@ class TestApi(unittest.TestCase):
         data = {
          "text": "Integration text example - GET"
         }
-        response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        response = requests.post(url, data=json.dumps(data))
         json_response = response.json()
         print('Response Add Todo: '+ str(json_response))
         jsonbody= json.loads(json_response['body'])
@@ -120,7 +120,7 @@ class TestApi(unittest.TestCase):
         data = {
          "text": "Integration text example - Initial"
         }
-        response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        response = requests.post(url, data=json.dumps(data))
         json_response = response.json()
         print('Response Add todo: ' + json_response['body'])
         jsonbody= json.loads(json_response['body'])
@@ -138,7 +138,7 @@ class TestApi(unittest.TestCase):
          "text": "Integration text example - Modified",
          "checked": "true"
         }
-        response = requests.put(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        response = requests.put(url, data=json.dumps(data))
         json_response = response.json()
         print('Response Update todo: ' + str(json_response))
         #jsonbody= json.loads(json_response['body'])
@@ -173,7 +173,7 @@ class TestApi(unittest.TestCase):
         data = {
          "text": "Integration text example - Initial"
         }
-        response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        response = requests.post(url, data=json.dumps(data))
         json_response = response.json()
         print('Response Add todo: ' + json_response['body'])
         jsonbody= json.loads(json_response['body'])
@@ -199,4 +199,3 @@ class TestApi(unittest.TestCase):
             response.status_code, 404, "Error en la petición API a {url}"
         )
         print('End - integration test Delete TODO')
-    
